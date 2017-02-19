5124ea1539e97449cbd5b5f1bfb35a8455643b2f
Rob Klouda <rgklouda@comcast.net>
2017-02-19 21:11:50 +0000

    Initial commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! JET LOAN <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
         src: 'src/factorial.js',
